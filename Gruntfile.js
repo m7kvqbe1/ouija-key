e13d212f969eca07f3099e71b8b93119ca559848
Houston VPS <tom@muska.co.uk>
2015-06-15 02:02:00 +0400

    Updated grunt build routine

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,48 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		
 		concat: {
 			options: {
-				banner: '/* Ouija Key, built <%=  grunt.template.today() %> */'
+				sourceMap: true,
+				separator: ';'
 			},
 
-			lib: {
-				src: [
-					'./application/vendor/*/*.js'
-				],
+			dist: {
+				src: ['./application/**/*.js', './application/*.js'],
 				
-				dest: './public/js/vendor.min.js'
+				dest: './public/js/app.js'
+			}
+		},
+		
+		uglify: {
+			options: {
+				banner: '/* <%= pkg.name %>, built <%=  grunt.template.today() %> */\n',
+				compress: true,
+				mangle: true,
+				sourceMap: true
 			},
-
-			app: {
-				src: [
-					'./application/components/*.js',
-					'./application/app.js'
-				],
-
-				dest: './public/js/app.min.js'
+			
+			dist: {
+				files: {
+					'./public/js/app.min.js': ['<%= concat.dist.dest %>']
+				}
+			}
+		},
+		
+		watch: {			
+			js: {
+				files: ['./application/**/*.js', './application/*.js'],
+				tasks: ['concat', 'uglify']
 			}
 		}
 	});
 
-	// Load the plugin that prvides the "concat" task.
+	// Load the plugins that prvide the various Grunt tasks
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// Default task(s).
-	grunt.registerTask('default', ['concat']);
+	grunt.registerTask('default', ['concat', 'uglify']);
 }
